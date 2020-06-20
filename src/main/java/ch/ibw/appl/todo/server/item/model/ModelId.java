2d51ff9e15256a8968acb2fe1c6e09c28e661e7c
Ueli Kunz <kunz@ideadapt.net>
2020-06-20 16:26:15 +0200

    implement equals and hashCode, so that HashMap.remove works correctly

--- src/main/java/ch/ibw/appl/todo/server/item/model/ModelId.java
+++ src/main/java/ch/ibw/appl/todo/server/item/model/ModelId.java
@@ -8,4 +8,19 @@ public class ModelId {
     model.id = id;
     return model;
   }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    ModelId modelId = (ModelId) o;
+
+    return id != null ? id.equals(modelId.id) : modelId.id == null;
+  }
+
+  @Override
+  public int hashCode() {
+    return id != null ? id.hashCode() : 0;
+  }
 }
